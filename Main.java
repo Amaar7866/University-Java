717951c3cb32f66b059dc00c8ebd99a7ad88bbe8
amaar7866 <amaar.ahmad@study.beds.ac.uk>
2024-08-27 15:22:51 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Variables to store your details
+        String firstName = "Amaar";
+        String lastName = "Ahmad";
+        String jobRole = "Jenkins Developer";
+
+        // Output your details
+        System.out.println("Name: " + firstName);
+        System.out.println("Surname: " + lastName);
+        System.out.println("Job Role: " + jobRole);
+    }
+}
